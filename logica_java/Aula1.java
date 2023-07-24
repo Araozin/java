f161d3dc451b3f7c026e9108b4464489ad3896f8
Arao Vitor <araodeveloper@gmail.com>
2023-07-24 14:11:05 -0300

    adcionando mudancas

--- logica_java/Aula1.java
+++ logica_java/Aula1.java
@@ -3,10 +3,10 @@ package logica_java;
 /**
  * aula1
  */
-public class aula1 {
+public class Aula1 {
 
     public static void main(String args[]) {
         System.out.println("Ola mundo");
-    }
 
+    }
 }
