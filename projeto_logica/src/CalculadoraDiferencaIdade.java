e3d76d476eea402ad710e00a86ccafd48439966a
Arao Vitor <araodeveloper@gmail.com>
2023-07-25 07:56:28 -0300

    Adicionado projeto calculadora

---
+++ projeto_logica/src/CalculadoraDiferencaIdade.java
@@ -0,0 +1,8 @@
+public class CalculadoraDiferencaIdade {
+    public static void main(String[] args){
+        int firstAge = 20;
+        int secondAge = 10;
+        int ageDiferenca = firstAge - secondAge;
+        System.out.println(ageDiferenca);
+    }
+}
